7b378a4ecb74aab0863322bdf641fb163fb9285f
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-03-31 13:53:20 -0400

    message class created

---
+++ src/main/java/Models/Message.java
@@ -0,0 +1,61 @@
+package Models;
+
+import java.util.Date;
+
+public class Message {
+    private long id;
+    private String channel;
+    private String sender;
+    private String msgContent;
+    private Date timestamp;
+
+    public Message (){};
+
+    public Message(long id, String channel, String sender, String msgContent, Date timestamp) {
+        this.id = id;
+        this.channel = channel;
+        this.sender = sender;
+        this.msgContent = msgContent;
+        this.timestamp = timestamp;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMsgContent() {
+        return msgContent;
+    }
+
+    public void setMsgContent(String msgContent) {
+        this.msgContent = msgContent;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+}
