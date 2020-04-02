7d5dd172ced87d2ba3d7f309e664bf56a17bbb7e
muhammeta7 <muhammeta7@gmail.com>
2020-04-02 12:05:01 -0400

    changed some things to objects

--- src/main/java/ZCW/ChatApp/models/Channel.java
+++ src/main/java/ZCW/ChatApp/models/Channel.java
@@ -30,11 +30,11 @@ public class Channel {
         this.messages = new ArrayList<>();
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
