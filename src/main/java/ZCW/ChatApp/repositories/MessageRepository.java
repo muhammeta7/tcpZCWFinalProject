0927f67369a00f8ab97a075bf23afcd49322da88
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-04-02 17:18:45 -0400

    created message service

--- src/main/java/ZCW/ChatApp/repositories/MessageRepository.java
+++ src/main/java/ZCW/ChatApp/repositories/MessageRepository.java
@@ -3,7 +3,13 @@ package ZCW.ChatApp.repositories;
 import ZCW.ChatApp.models.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sun.jvm.hotspot.debugger.Page;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+     List<Message> findMessageBySender(String sender, Pageable pageable);
+
 }
--- src/main/java/ZCW/ChatApp/services/MessageService.java
+++ src/main/java/ZCW/ChatApp/services/MessageService.java
@@ -1,7 +1,46 @@
 package ZCW.ChatApp.services;
 
+
+import ZCW.ChatApp.models.Message;
+import ZCW.ChatApp.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class MessageService {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    public Message create(Message message){
+        message.setTimestamp(new Date());
+        return messageRepository.save(message);
+    }
+    public Iterable<Message> findAll(){
+        return messageRepository.findAll();
+    }
+
+
+    public List<Message> findBySender(String sender, Pageable pageable){
+        return messageRepository.findMessageBySender(sender, pageable);
+    }
+
+    public Message findById(Long id){
+        return messageRepository.getOne(id);
+    }
+
+    public Boolean delete(Long id){
+        messageRepository.deleteById(id);
+        return true;
+    }
+
+    public Boolean deleteAll(){
+        messageRepository.deleteAll();
+        return true;
+    }
+
 }
