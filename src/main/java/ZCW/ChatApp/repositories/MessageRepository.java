18dd279b1c2d23c553fe7137ce03275c46e39584
muhammeta7 <muhammeta7@gmail.com>
2020-04-05 22:51:08 -0400

    removed useless code

--- src/main/java/ZCW/ChatApp/repositories/MessageRepository.java
+++ src/main/java/ZCW/ChatApp/repositories/MessageRepository.java
@@ -1,16 +1,11 @@
 package ZCW.ChatApp.repositories;
 
 import ZCW.ChatApp.models.Message;
-import ZCW.ChatApp.models.User;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-   List<Message> findMessageBySender(User sender, Pageable pageable);
-   // TODO Add find message by Date
 
 }
