a7cb690cc529be4a9c82d1fd17ebbdc8bb95da1c
muhammeta7 <muhammeta7@gmail.com>
2020-04-11 02:19:42 -0400

    fixed controller tests

--- src/test/java/ZCW/ChatApp/controllers/UserControllerTest.java
+++ src/test/java/ZCW/ChatApp/controllers/UserControllerTest.java
@@ -210,10 +210,43 @@ public class UserControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
 
                 .andExpect(jsonPath("$.connected", is(false)));
-
     }
 
+    @Test
+    @DisplayName("PUT /users/update/username/1 - Success")
+    void updateUserNameSuccessTest() throws Exception {
+        Long givenId = 1L;
+        User putUser = new User(1L,"Moe", "Aydin", "muhammeta7", "password", true);
+        String newUsername = "anything";
+        given(userService.updateUserName(putUser.getId(), newUsername)).willReturn(Optional.of(putUser));
 
+        mockMvc.perform(put("/users/update/username/{id}", givenId)
+                .header(HttpHeaders.IF_MATCH, 1)
+                .param("username", newUsername))
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+
+                .andExpect(jsonPath("$.userName", is("muhammeta7")));
+    }
+
+    @Test
+    @DisplayName("PUT /users/update/password/1 - Success")
+    void updatePasswordSuccessTest() throws Exception {
+        Long givenId = 1L;
+        User putUser = new User(1L,"Moe", "Aydin", "muhammeta7", "password", true);
+        String newPassword = "anything";
+        given(userService.updatePassword(putUser.getId(), newPassword)).willReturn(Optional.of(putUser));
+
+        mockMvc.perform(put("/users/update/password/{id}", givenId)
+                .header(HttpHeaders.IF_MATCH, 1)
+                .param("password", newPassword))
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+
+                .andExpect(jsonPath("$.password", is("password")));
+    }
 
     // TODO join channel
     // TODO leave channel
